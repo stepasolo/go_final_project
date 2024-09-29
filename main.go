3aa16a694865044a9b776814e8b62ffe1be18881
Dmitriy P <cool.pominov2015@yandex.com>
2024-09-29 11:59:03 +0300

    add nextDate func

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	dbHelper "github.com/LingL42/finalGoProject/db"
 )
@@ -11,6 +12,7 @@ import (
 func main() {
 	dbHelper.DbWorker()
 	fmt.Println("Zapuskaem server")
+	http.HandleFunc("/api/nextdate", nextDateHandler)
 	//port := fmt.Sprintf(`:%s`, os.Getenv("TODO_PORT"))
 	port := fmt.Sprintf(`0.0.0.0:%s`, os.Getenv("TODO_PORT")) //для работы через WSL
 	fmt.Println(port)
@@ -22,4 +24,35 @@ func main() {
 		panic(err)
 	}
 
+}
+
+func nextDateHandler(w http.ResponseWriter, r *http.Request) {
+	nowStr := r.URL.Query().Get("now")
+	dateStr := r.URL.Query().Get("date")
+	repeatStr := r.URL.Query().Get("repeat")
+
+	now, err := time.Parse("20060102", nowStr)
+	if err != nil {
+		http.Error(w, "Неверный now", http.StatusBadRequest)
+		return
+	}
+
+	nextDate, err := NextDate(now, dateStr, repeatStr)
+	response := Response{}
+
+	if err != nil {
+		response.Error = err.Error()
+	} else {
+		response.Result = nextDate
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	//json.NewEncoder(w).Encode(response)
+	fmt.Fprint(w, nextDate)
+
+}
+
+type Response struct {
+	Result string `json:"result"`
+	Error  string `json:"error,omitempty"`
 }
